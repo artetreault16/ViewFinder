bab2a28b2cbd7bda1008bbdab05efad676e32879
Apple <apple@Apples-MacBook-Pro-38.local>
2019-06-11 13:53:16 -0700

    Iteration 1

---
+++ viewFinder/AddPhotoViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddPhotoViewController.swift
+//  viewFinder
+//
+//  Created by Apple on 6/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var newImage: UIImageView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoLibrary(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoAlbum(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            newImage.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
